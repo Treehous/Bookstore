5cfabd79db3d929019c0b354c085d9c485b1395c
Patrick Lynn <plynn@ycp.edu>
2016-04-02 20:52:59 -0400

    rename

--- src/servlet/FrontEndServlet.java
+++ src/servlet/FrontEndServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FrontServlet extends HttpServlet {
+public class FrontEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	@Override
