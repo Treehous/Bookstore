9fafef3369a97e05b1adba256bcc4155e822421a
Patrick Lynn <plynn@ycp.edu>
2016-04-13 11:56:23 -0400

    database Update

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 import database.DatabaseProvider;
 import database.IDatabase;
-import model.BookstoreModel;
 import src.Author;
 import src.Book;
 
 public class SearchController {
-	private BookstoreModel model;
 	private IDatabase database;
-	
-	public SearchController() {
-		this.model = new BookstoreModel();
-	}
 	
 	public List<Book> getBooksByAuthor(List<Author> authors){
 		this.database = DatabaseProvider.getDatabase();
